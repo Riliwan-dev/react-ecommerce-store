3409eeef743a7eba647547d4acd9a5696eb5dcd7
Riliwan-dev <genius.rio121@gmail.com>
2025-10-15 22:06:26 +0100

    Modification of Delete.jsx file

--- ecommerce-project/src/components/delete.jsx
+++ ecommerce-project/src/components/delete.jsx
@@ -1,22 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Chatbot Example</title>
-</head>
-<body>
-  
-  <div id="hey"></div>
 
-  <!-- ✅ Official React 18 -->
-  <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
-  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
-
-  <!-- ✅ Babel (needed to compile JSX) -->
-  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-
-  <script type="text/babel">
      function ChatInput() {
       return (
         <>
